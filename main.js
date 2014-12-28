e17203b5c11f35c5987bfe9ce8b35029c2aba840
laike9m <laike9m@gmail.com>
2014-12-28 23:01:08 +0800

    some changes

--- main.js
+++ main.js
@@ -1,23 +1,5 @@
 var fs = require('fs');
 
-function toArrayBuffer(buffer) {
-  var ab = new ArrayBuffer(buffer.length);
-  var view = new Uint8Array(ab);
-  for (var i = 0; i < buffer.length; ++i) {
-    view[i] = buffer[i];
-  }
-  return ab;
-}
-
-function toBuffer(ab) {
-  var buffer = new Buffer(ab.byteLength);
-  var view = new Uint8Array(ab);
-  for (var i = 0; i < buffer.length; ++i) {
-    buffer[i] = view[i];
-  }
-  return buffer;
-}
-
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -30,11 +12,9 @@ app.get('/', function (req, res) {
 
 io.on('connection', function(socket){
   socket.on('send', function(data){
-    data = fs.readFileSync('.gitignore');
-    socket.emit('sendToPeer', toArrayBuffer(data));
+    socket.emit('sendToPeer', fs.readFileSync('.gitignore'));
   });
   socket.on('receive', function(data){
-    console.log("start wrting to file");
     fs.writeFileSync('received_gitignore', data);
   });
 });
